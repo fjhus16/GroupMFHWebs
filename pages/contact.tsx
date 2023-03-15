7bb1b222ac9210e5b7a94aeb57600f90ddd6c813
fjhus16 <manyakgek123@hotmail.com>
2023-03-15 18:15:20 +0300

    making it look better

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import Container from '../components/container'
 import Layout from '../components/layout'
 import Head from 'next/head'
+import { TextField } from '@mui/material'
 
 export default function Contact() {
   return (
@@ -10,21 +11,17 @@ export default function Contact() {
           <title>{`GroupMFH`}</title>
         </Head>
         <Container>
-          <div className='py-2 px-2 bg-gray-400 mt-10 mx-auto md:w-[60vw]'>
-            <form className='flex grid grid-cols-2 md:w-[60vw] justify-center items-center flex-center' action="https://formsubmit.co/fce71d935be63f864888db98ccb0ccae" method="POST">
-              <label className='text-2xl font-bold' htmlFor="isim">İsminiz*</label>
-              <input className='my-2 border-solid mx-auto w-[80%] justify-center items-center align-center' type="Name" name="isim" required />
-              <label className='text-2xl font-bold' htmlFor="eposta">E-posta adresiniz*</label>
-              <input className='my-2 border-solid mx-auto w-[80%] justify-center items-center align-center' type="email" name="email" required />
-              <label className='text-2xl font-bold' htmlFor="telefon">Telefon numaranız</label>
-              <input className='my-2 border-solid mx-auto w-[80%] justify-center items-center align-center' type="Phone" name="telefon" />
-              <label className='text-2xl font-bold' htmlFor="şirket">Şirketinizin adı</label>
-              <input className='my-2 border-solid mx-auto w-[80%] justify-center items-center align-center' type="Şirketinizin adı" name="şirket" />
+          <div className='my-2 pb-4 pt-2 border rounded-md mt-10 mx-auto md:w-[40vw]'>
+            <form className='flex pt-5 gap-5 flex-col md:w-[40vw] justify-center items-center' action="https://formsubmit.co/fce71d935be63f864888db98ccb0ccae" method="POST">
+              <TextField className='w-[90%]' id="outlined-basic" label="İsminiz" name="isim" variant="outlined" />
+              <TextField className='w-[90%]' id="outlined-basic" label="E-posta adresiniz" name="email" variant="outlined" />
+              <TextField className='w-[90%]' id="outlined-basic" label="Telefon numaranız" name="telefon" variant="outlined" />
+              <TextField className='w-[90%]' id="outlined-basic" label="Şirketinizin adı" name="şirket" variant="outlined" />
+              <TextField className='w-[90%]' id="outlined-basic" label="Mesajınız" name="mesaj" variant="outlined" />
               <input type="text" name="_honey" className="hidden" />
               <input type="hidden" name="_captcha" value="false" />
-              <label className='text-2xl font-bold' htmlFor="mesaj">Mesajınız*</label>
-              <input className='multiline my-2 border-solid mx-auto w-[80%] justify-center items-center align-center' type="Message" name="mesaj" required />
-              <button className='col-span-2 bg-gray-800 text-white' type="submit">Gönder</button>
+              <input type="hidden" name="_template" value="box" />
+              <button className='w-[20%] py-2 rounded bg-gray-800 text-white' type="submit">Gönder</button>
             </form>
           </div>
         </Container>
