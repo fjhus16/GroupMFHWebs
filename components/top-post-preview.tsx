b8a0d7c78530031984297994de5077afd76c9928
fjhus16 <manyakgek123@hotmail.com>
2023-04-01 00:58:54 +0300

    img optimization

--- components/top-post-preview.tsx
+++ components/top-post-preview.tsx
@@ -23,7 +23,7 @@ const TopPostPreview = ({
 
   return (
     <div className='h-[400px] flex flex-col items-center justify-center'>
-     <Image fill style={{objectFit:"cover"}} src={coverImage} alt={title} className='-z-10'/>
+     <Image fill style={{objectFit:"cover"}} priority loading='eager' src={coverImage} alt={title} className='-z-10'/>
       <div className='flex flex-col items-center justify-center text-white'>
       <h3 className="font-bold text-3xl mb-3 w-[85%] md:w-[65%] [text-shadow:_3px_3px_3px_rgb(0_0_0_/_80%)]">
         <Link
