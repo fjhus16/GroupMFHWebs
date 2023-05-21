f74e705bbc6c2ae1aabe1ddac6f2adbd460fe953
fjhus16 <manyakgek123@hotmail.com>
2023-05-21 03:25:35 +0300

    bug

--- components/tr/about-menu.tsx
+++ components/tr/about-menu.tsx
@@ -16,23 +16,12 @@ export default function AboutMenu() {
                         <p className={cormorantGaramond.className}>Kurumsal</p>
                     </Menu.Button>
                 </div>
-                <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-200"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                >
+                <Transition as={Fragment} enter="transition ease-out duration-200" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-200" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
                     <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="px-1 py-1">
                             <Menu.Item>
                                 {({ active }) => (
-                                    <button
-                                        className={`${active ? 'bg-black text-white' : 'text-gray-900'
-                                            } duration-1000 group flex w-full items-center px-2 py-2 text-sm`}
-                                    >
+                                    <button className={`${active ? 'bg-black text-white' : 'text-gray-900'} duration-1000 group flex w-full items-center px-2 py-2 text-sm`}>
                                         Hakkımızda
                                     </button>
                                 )}
@@ -41,10 +30,7 @@ export default function AboutMenu() {
                         <div className="px-1 py-1">
                             <Menu.Item>
                                 {({ active }) => (
-                                    <button
-                                        className={`${active ? 'bg-black text-white' : 'text-gray-900'
-                                            } duration-1000 group flex w-full items-center px-2 py-2 text-sm`}
-                                    >
+                                    <button className={`${active ? 'bg-black text-white' : 'text-gray-900'} duration-1000 group flex w-full items-center px-2 py-2 text-sm`}>
                                         Değerlerimiz
                                     </button>
                                 )}
@@ -53,10 +39,7 @@ export default function AboutMenu() {
                         <div className="px-1 py-1">
                             <Menu.Item>
                                 {({ active }) => (
-                                    <button
-                                        className={`${active ? 'bg-black text-white' : 'text-gray-900'
-                                            } duration-1000 group flex w-full items-center px-2 py-2 text-sm`}
-                                    >
+                                    <button className={`${active ? 'bg-black text-white' : 'text-gray-900'} duration-1000 group flex w-full items-center px-2 py-2 text-sm`}>
                                         Ekibimiz
                                     </button>
                                 )}
