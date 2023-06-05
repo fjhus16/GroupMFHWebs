815d30f665bdd8cec23d84c7407711d57ee7e4ef
fjhus16 <manyakgek123@hotmail.com>
2023-06-05 18:57:23 +0300

    belgrade

--- pages/en/belgrade.tsx
+++ pages/en/belgrade.tsx
@@ -6,13 +6,82 @@ import Head from 'next/head'
 export default function Belgrade() {
   return (
     <>
-    <Layout>
-      <Head>
-        <title>GroupMFH</title>
-      </Head>
+      <Layout>
+        <Head>
+          <title>GroupMFH</title>
+        </Head>
         <Container>
           <section className="flex-col flex items-center md:justify-between pt-6 pb-6">
-            
+            <h2 className="mb-2 pt-6 text-5xl md:text-5xl font-bold tracking-tighter leading-tight">
+              Information About Company Esatblishment in Serbia
+            </h2>
+            <p>
+              <p className='font-bold'>1. Types of Companies</p>
+              The Serbian Company Law regulates companies under four categories, and both domestic and foreign individuals or legal entities interested in doing business in Serbia have the right to establish any of these company types or become a partner in an existing company.
+              <ul>
+                <li>- Limited Liability Company (Društvo s ograničenom odgovornošću, d.o.o.)</li>
+                <li>- Joint-Stock Company (Akcionarsko društvo, a.d.)</li>
+                <li>- General Partnership (Ortačko društvo, o.d.)</li>
+                <li>- Limited Partnership (Komanditno društvo, k.d.)</li>
+              </ul><br />
+              Additionally, commercial activities can also be conducted through branch offices and representative offices.<br />
+              <p className='font-bold'>1.1. Limited Liability Company</p>
+              The limited liability company is the most preferred company type by small and medium-sized domestic and foreign entrepreneurs in Serbia. It can be established by one or more individuals or legal entities, with a maximum of 50 shareholders. The liability of the shareholders is limited to their capital contributions. Limited liability companies require a minimum registered capital of at least 100 RSD (approximately 1 Euro). The capital can be paid in cash or in kind.
+              In limited liability companies, the director is the legal representative of the company. The director is not required to be a Serbian citizen or have a residence permit in Serbia. Additionally, the director can also be one of the company shareholders.
+              <p className='font-bold'>1.2. Joint-Stock Company</p>
+              A joint-stock company is a company with a specified capital divided into shares, where shareholders are only liable to the extent of their commitments to the company. Compared to other types of companies, it is the most complex to establish. It can be established by one or more individuals or legal entities.
+              During the establishment phase of the company, shareholders of a joint-stock company sign the articles of association and the company's bylaws. Joint-stock companies require a minimum registered capital of at least 3,000,000 RSD (approximately 25,000 Euros). The capital can be paid in cash or in kind.
+              <p className='font-bold'>1.3. General Partnership</p>
+              A general partnership is a company established by two or more individuals or legal entities, where all partners are jointly and severally liable for all debts and obligations of the company. There is no minimum capital requirement for general partnerships.
+              <p className='font-bold'>1.4. Limited Partnership</p>
+              A limited partnership is established by at least two partners, one being a general partner with unlimited liability for the company's debts, and the other being a limited partner with liability limited to the capital contributed. Two or more individuals can be partners in a limited partnership. There is no minimum capital requirement for limited partnerships.
+              <p className='font-bold'>1.5. Others</p>
+              Foreign investors interested in operating in Serbia can also conduct their activities through branch offices or representative offices, in addition to the mentioned company types.
+              Opening a branch office of a foreign company is possible in Serbia, but since branches are subject to registration, the process is not significantly different from establishing a new company. A foreign branch is defined as a separate organizational unit of a company engaged in commercial activities in accordance with the law. The branch does not have a separate legal personality but can conduct activities on behalf of the company.
+              Foreign companies can also open a representative office in Serbia. The representative office does not have a separate legal personality from the parent company in Turkey. The representative office primarily carries out supportive activities (advertising, product promotion, finding customers, etc.) on behalf of the parent company in Serbia. Representative offices are not allowed to engage in commercial activities, and therefore, they are not subject to corporate tax obligations.
+              <p className='font-bold'>2. Company Formation Procedure</p>
+              All companies established by domestic or foreign individuals or legal entities in Serbia are required to register with the Serbian Business Registers Agency (APR). While the registration process typically takes about seven business days, it is important to follow the application steps properly to avoid any delays.
+              Companies established by foreign individuals or legal entities are considered Serbian companies regardless of whether they have a Serbian partner or not, and they operate under the same conditions as domestic companies. Additionally, it is not necessary for foreign individuals or representatives of foreign legal entities to be present in Serbia to complete the company establishment. Our team provides detailed guidance on the necessary procedures for a company to be fully established and operational.
+              The company formation procedure in Serbia is conducted through the Serbian Business Registers Agency, and the steps to be followed during the company establishment process are as follows:
+              <p className='font-bold'>2.1. Selection of Company Type</p>
+              To establish a company in Serbia, one of the mentioned company types must be selected. The establishment costs vary depending on the chosen company type.
+              <p className='font-bold'>2.2. Selection of Trade Name</p>
+              Trade names should include certain essential elements such as the company's name, legal form, and registered office location. Additionally, trade names should comply with restrictions specified in laws, regulations, and guidelines.
+              Trade names should be unique and not identical or too similar to existing names. This can be checked through the records of the Serbian Business Registers Agency.
+              <p className='font-bold'>2.3. Selection of Activity Code</p>
+              All legal entities and commercial organizations are required to select their activity codes (the main activities they will primarily engage in). The activity code represents the main activity through which a company primarily generates profit. Selecting the main activity does not prevent the company from conducting other activities unless otherwise specified.
+              <p className='font-bold'>2.4. Declaration of Address</p>
+              Instead of a physical office, it is possible to lease a virtual office and register the company's main office at that address, independent of the location where the activities are carried out.
+              Virtual offices are used to register a company's address, receive mail, provide administrative support to companies, answer phone calls, etc. These offices offer several advantages, particularly for newly established companies, including cost savings and flexibility, as they can operate independently of the main activity center.
+              <p className='font-bold'>2.5. Preparation of Incorporation Decision</p>
+              The incorporation decision (articles of association in the case of a single-partner company or the founding agreement in the case of multiple partners) needs to be notarized.
+              The company's incorporation decision should include the following information: company name, registered address, main activity type (NACE code), capital amount, names of company partners, their identification/passport numbers, addresses (in the case of a partner being another company, the company name, tax number, country of registration, and registry entry), shares of each partner, the capital amount of each partner, name of company representative, their identification/passport number, address, whether the company's establishment is limited in duration, information about persons authorized to sign on behalf of the company, information about the supervisory board and auditor if applicable, etc.
+              <p className='font-bold'>2.6. Opening a Temporary Bank Account</p>
+              A temporary account needs to be opened at a bank, and registration fees must be paid. The temporary account opened at the bank will be activated with the documentation obtained from the Serbian Business Registers Agency.
+              <p className='font-bold'>2.7. Applying to the Serbian Employment Agency, Documents required for the application:</p>
+              <ul>
+                <li>•	Application registration form</li>
+                <li>•	Signature circulars of the company shareholders and notarized company establishment decision</li>
+                <li>•	Photocopy of passport for foreign citizens, photocopy of identity document if there is a Serbian citizen partner, certified copy of the trade register if the founder is a legal entity</li>
+                <li>•	Bank approval for the capital to be paid in cash before incorporation</li>
+                <li>• Payment receipts for company establishment fees</li>
+                <li>•	Power of attorney if the application is made by proxy</li>
+              </ul>
+              Once the approval process is completed, the company registration is published on the APR's official website. A printed copy of the APR's Decision on company registration is provided to the company's authorized person upon request. Such request must be specified separately at the time of application.
+              <p className='font-bold'>2.8. Sworn Translation</p>
+              All documents in a foreign language used in the application must be translated into Serbian by a sworn translator.
+              <p className='font-bold'>2.9. Opening a Commercial Bank Account</p>
+              In case the founders/shareholders will not pay the founding capital before registering the company, they are required to open a bank account in one of the banks in the Republic of Serbia and deposit the capital after the company registration is completed.
+              <p className='font-bold'>2.10. Obtaining Tax Identification Number and Registration Number</p>
+              After the company registration is completed, it is necessary to obtain a tax identification number (PIB) from the Tax Administration of Serbia (PURS) and a registration number from the Statistical Office of Serbia.
+              <p className='font-bold'>2.11. Filing Social Security Applications</p>
+              Necessary applications must be made for compulsory insurance and other social security payments (social insurance, health insurance and unemployment insurance) for company partners and employees.
+              <p className='font-bold'>2.12. Stamp and Seal</p>
+              Depending on the need, a sufficient number of official stamps of the company should be printed. On the other hand, although it is not mandatory, in practice, it is still requested to seal documents such as contracts, etc., so it is also beneficial to have a seal printed.
+              <p className='font-bold'>2.13. Provision of qualified electronic signature/certificate</p>
+              A qualified electronic signature is required for electronic signing, submission and approval of documents, and an electronic certificate is required for the submission of the company's annual financial statements and tax returns.
+              We assume that you need a reliable partner to set up a company and provide support related to accounting, tax and payroll services. You can contact our team who will assist you in these matters and all other activities.
+            </p>
           </section>
           <ContactForm />
         </Container>
--- pages/tr/belgrade.tsx
+++ pages/tr/belgrade.tsx
@@ -12,8 +12,78 @@ export default function Belgrade() {
         </Head>
         <Container>
           <section className="flex-col flex items-center md:justify-between pt-6 pb-6">
-            
-         </section>
+            <h2 className="mb-2 pt-6 text-5xl md:text-5xl font-bold tracking-tighter leading-tight">
+              Sırbistan'da Şirket Kuruluşu İle İlgili Bilgiler
+            </h2>
+           <p> 
+            <p className='font-bold'>1. Şirket Türleri</p>         
+            Sırbistan Şirketler Kanunu, şirketleri dört başlıkta düzenlemekte olup Sırbistan’da iş yapmak isteyen yerli veya yabancı uyruklu tüm gerçek ve tüzel kişiler, bu şirket türlerinden herhangi birini kurma veya kurulu olan bir şirkete ortak olma hakkına sahiptirler.<br /><br />
+            <ul>
+              <li>- Limited Şirket (Društvo s ograničenom odgovornošću, d.o.o.)</li>
+              <li>-	Anonim Şirket (Akcionarsko društvo, a.d.)</li>
+              <li>-	Kolektif Şirket (Ortačko društvo, o.d.)</li>
+              <li>-	Komandit Şirket (Komanditno društvo, k.d.)</li>
+            </ul><br />
+            Bunlara ek olarak ticari faaliyetler şube ve temsilcilik ofisi şeklinde de yürütülebilmektedir.<br/>
+            <p className='font-bold'>1.1. Limited Şirket</p>
+            Limited şirket Sırbistan’da küçük ve orta ölçekli yerli ve yabancı girişimcilerin en çok tercih ettiği şirket türüdür. Bir veya daha çok gerçek veya tüzel kişi tarafından kurulabilmektedir. Ortak sayısı en fazla 50 olabilir. Ortakların sorumlulukları sermayeleri ile sınırlıdır. Limited şirketlerde asgari kayıtlı sermaye tutarının en az 100 RSD (yaklaşık 1 Avro) olması gerekir. Şirket sermayesinin nakdi veya ayni olarak ödenmesi mümkündür.
+            Limited şirketlerde yönetici, şirketin yasal temsilcisidir. Şirket müdürünün Sırbistan vatandaşı olması veya Sırbistan’da oturum iznine sahip olması şartı bulunmamaktadır. Ayrıca şirket müdürü, şirket ortaklarından birisi de olabilir.
+            <p className='font-bold'>1.2. Anonim Şirket</p>
+            Anonim şirket, sermayesi belirli ve paylara bölünmüş, ortakların yalnızca şirkete karşı taahhüt ettikleri miktarlarla sınırlı olarak sorumlu oldukları şirket türüdür. Diğer şirket türleri ile karşılaştırıldığında kuruluşu en karmaşık olan şirket türüdür. Bir veya daha çok gerçek veya tüzel kişi tarafından kurulabilmektedir.
+            Şirketin kuruluş aşamasında şirket hissedarları kuruluş sözleşmesini ve şirket ana sözleşmesini imzalar. Anonim şirketlerde asgari kayıtlı sermaye tutarının en az 3.000.000 RSD (yaklaşık 25.000 Avro) olması gerekir. Şirket sermayesinin nakdi veya ayni olarak ödenmesi mümkündür.
+            <p className='font-bold'>1.3. Kolektif Şirket</p>
+            Kolektif şirket iki veya daha fazla gerçek veya tüzel kişi tarafından kurulan ve bu kişilerin şirketin tüm borç ve alacaklarından bütün varlıkları ile sorumlu olduğu şirkettir. Kolektif şirketler için asgari sermaye şartı bulunmamaktadır.
+            <p className='font-bold'>1.4. Komandit Şirket</p>
+            Komandit şirket biri komandite diğeri komanditer olmak üzere en az iki ortak tarafından kurulur. Komandite ortak, şirket borçlarına karşı sınırsız sorumluluğu olan ortaktır. Komanditer ortak ise sorumluğu şirkete koyduğu sermaye ile sınırlı olan ortaktır.  Komandit şirkete iki veya daha fazla kişi ortak olabilmektedir. Asgari sermaye şartı bulunmamaktadır.
+            <p className='font-bold'>1.5. Diğer</p>
+            Sırbistan’da faaliyette bulunmak isteyen yabancı yatırımcılar, yukarıda belirtilen şirket türlerine ilaveten şube veya temsilcilik ofisi biçiminde de faaliyet gösterebilmektedir.
+            Sırbistan’da yabancı bir firmanın şubesinin açılması mümkündür, fakat şubelerin de tescil zorunluluğu bulunduğundan bu işlem yeni bir şirket açılmasından çok farklı bir süreç değildir. Yurt dışı şube, yasalara uygun olarak ticari faaliyette bulunan bir şirketin ayrı bir organizasyon birimi olarak tanımlanmıştır. Şubenin ayrı bir tüzel kişiliği bulunmaz, ancak şirket adına faaliyetlerini yürütebilir.
+            Yabancı şirketler Sırbistan’da bir temsilcilik ofisi de açabilir. Temsilcilik ofisinin Türkiye’deki şirketten ayrı bir tüzel kişiliği bulunmamaktadır. Temsilcilik ofisi temel olarak Türkiye’deki ana şirket adına Sırbistan’da destekleyici faaliyetler (reklam, ürünlerin pazarlanması, müşteri bulma vb.) yürütür. Temsilcilik ofislerinin ticari faaliyette bulunmasına izin verilmez, bu nedenle kurumlar vergisi ödeme sorumluluğu bulunmamaktadır.
+            <p className='font-bold'>2. Şirket Kurma Prosedürü</p>
+            Sırbistan’da yerli veya yabancı gerçek veya tüzel kişiler tarafından kurulan tüm şirketlerin, Sırp İş Kayıt Ajansı’na (APR) kayıt olma zorunluluğu bulunmaktadır. Şirketlerin kayıt işlemleri yaklaşık yedi iş günü içinde tamamlansa da bu sürenin uzamaması için başvuru adımlarının uygun şekilde yapılması gerekmektedir.
+            Yabancı gerçek veya tüzel kişiler tarafından kurulan şirketler, Sırp ortağı olup olmadığına bakılmaksızın Sırp şirketi olarak kabul edilmekte ve yerli şirketler ile aynı koşullarda faaliyet yürütmektedir. Ayrıca, yabancı gerçek kişilerin veya yabancı tüzel kişilerin temsilcilerinin şirketin kuruluşunu tamamlamak için Sırbistan’da bulunmaları gerekmemektedir. Bu kapsamda, bir şirketin kurulup tamamen faaliyete geçmesi için gerekli olan işlemlere ilişkin detaylı rehberlik ekibimiz tarafından sağlanmaktadır.
+            Sırbistan’da şirket kurma prosedürü Sırp İş Kayıt Ajansı üzerinden yürütülmekte olup, şirket kuruluş sürecinde izlenecek adımlar aşağıda yer almaktadır:
+            <p className='font-bold'>2.1. Şirket Türünün Seçilmesi</p>
+            Sırbistan’da şirket kurabilmek için öncelikle yukarıda sayılan şirket türlerinden birinin seçilmesi gerekmektedir. Kuruluş maliyetleri seçilen şirket türüne göre değişmektedir.
+            <p className='font-bold'>2.2. Ticaret Unvanının Seçilmesi</p>
+            Ticaret unvanlarının şirketin adı, yasal şekli ve merkezinin kayıtlı olduğu yer gibi bazı temel unsurları içermesi gerekmektedir. Bunun yanı sıra ticaret unvanlarının; kanunlarda, yönergelerde ve diğer düzenlemelerde yer alan kısıtlamalara da uygun olması gerekmektedir.
+            Ticaret unvanlarının benzersiz olması ve mevcut bir unvanla aynı veya çok benzer olmaması gerekmektedir. Bunun kontrolü Sırp İş Kayıt Ajansı kayıtları üzerinden yapılabilmektedir.
+            <p className='font-bold'>2.3. Faaliyet Kodunun Seçilmesi</p>
+            Tüm tüzel kişilerin ve ticari kuruluşların faaliyet kodlarını (esas olarak yapacakları faaliyet) seçmesi gerekmektedir. Faaliyet kodu, bir firmanın kâr elde etmek için çoğunlukla yapacağı ana faaliyetini ifade etmektedir. Ana faaliyet konusunun seçilmesi, aksi öngörülmemidiği sürece diğer faaliyetlerin yürütülmesine engel olmamaktadır.
+            <p className='font-bold'>2.4. Adres Beyanı</p>
+            Fiziki ofis yerine sanal ofis kiralamak ve faaliyetlerin gerçekleştirildiği yerden bağımsız olarak şirketin ana ofisini bu adrese kaydettirmek mümkündür.
+            Sanal ofisler; şirket adresini kaydetmek ve postaları kabul etmek, firmalara idari destek vermek, telefonları cevaplamak vb. amaçlar için kullanılmaktadır. Bu ofisler esnek yapıları ve esas faaliyet merkezinden bağımsız olabilmeleri sebebiyle yeni kurulan şirketler için başta maliyet olmak üzere birçok avantaj sağlamaktadır.
+            <p className='font-bold'>2.5. Kuruluş Kararının Hazırlanması</p>
+            Kuruluş kararının (şirketin tek ortaklı kurulması durumunda kuruluş sözleşmesi, birden fazla ortak ile kurulması durumunda kuruluş anlaşması) notere tasdik ettirilmesi gerekmektedir.
+            Şirket kuruluş kararında; şirketin adı, merkez adresi, ana faaliyet türü (NACE kodu), sermaye miktarı, şirket ortaklarının adı, soyadı, kimlik/pasaport numarası, adresi (şirket ortağının bir başka şirket olması durumunda şirket unvanı, vergi numarası, merkezinin bulunduğu ülke, sicil kaydı), ortakların payları, her bir ortağın sermaye miktarı, şirket temsilcisinin adı, soyadı, kimlik/pasaport numarası, adresi, şirket kuruluş süresinin sınırlı olup olmadığı, imza yetkisine sahip kişilere ilişkin bilgiler, eğer atanması gerekiyorsa denetim kurulu ve murakıba ilişkin bilgiler vb. bilgilerin yer alması gerekmektedir.
+            <p className='font-bold'>2.6. Geçici Banka Hesabının Açılması</p>
+            Bankada geçici bir hesap açtırılması ve kayıt ücretlerinin ödenmesi gerekmektedir. Bankada açılan geçici hesap, Sırbistan İş Kayıt Ajansı’ndan alınan evrak ile aktif hale getirilecektir.
+            <p className='font-bold'>2.7. Sırbistan İş Kayıt Ajansı’na Başvuru Yapılması Başvuru için gerekli belgeler:</p>
+            <ul>
+              <li>•	Başvuru kayıt formu</li>
+              <li>•	Şirket hissedarlarının imza sirküleri ile notere tasdik ettirilmiş şirket kuruluş kararı</li>
+              <li>•	Yabancı ülke vatandaşları için pasaport fotokopisi, Sırp vatandaşı ortak var ise kimlik belgesi fotokopisi, kurucu tüzel kişilik ise ticaret sicil kaydının onaylı örneği</li>
+              <li>•	Kuruluştan önce nakit ödenecek sermayeye ilişkin banka onayı</li>
+              <li>• Şirket kuruluş harçlarına ilişkin ödeme makbuzları</li>
+              <li>•	Eğer başvuru vekalet ile yapıldıysa vekaletname</li>
+            </ul>
+            Onay süreci tamamlandığında, şirket kaydı APR’nin resmi web sayfasında yayımlanmaktadır. APR’nin şirket kaydına ilişkin aldığı Karar’ın basılı bir nüshası, talep edilmesi halinde şirket yetkilisine verilmektedir. Söz konusu talebin başvuru sırasında ayrıca belirtilmesi gerekmektedir.
+            <p className='font-bold'>2.8. Yeminli Tercüme</p>
+            Başvuru sırasında kullanılan yabancı dildeki tüm belgelerin, yeminli tercüman tarafından Sırpça’ya çevirisinin yapılması gerekmektedir.
+            <p className='font-bold'>2.9. Ticari Banka Hesabının Açılması</p>
+            Kurucuların/hissedarların şirketi tescil ettirmeden önce kuruluş sermayesini ödemeyecek olmaları halinde, şirket kaydı tamamlandıktan sonra Sırbistan Cumhuriyeti’ndeki bankalardan birinde bir banka hesabı açmaları ve sermayeyi yatırmaları gerekmektedir.
+            <p className='font-bold'>2.10. Vergi Kimlik Numarası ve Sicil Numarası Alınması</p>
+            Şirket kaydı tamamlandıktan sonra Sırbistan Vergi İdaresi’nden (PURS) bir vergi kimlik numarası (PIB) ve Sırbistan İstatistik Ofisi’nden bir sicil numarası alınması gerekmektedir.
+            <p className='font-bold'>2.11. Sosyal Güvenlik Başvurularının Yapılması</p>
+            Şirket ortakları ile çalışanlar için zorunlu sigortaların ve diğer sosyal güvenlik ödemelerinin
+            (sosyal sigorta, sağlık sigortası ve işsizlik sigortası) yapılabilmesi için gerekli başvuruların yapılması gerekmektedir.
+            <p className='font-bold'> 2.12. Kaşe ve Mühür</p>
+            İhtiyaca göre yeterli sayıda şirkete ait resmi kaşenin bastırılması gerekmektedir. Öte yandan, zorunlu olmamakla birlikte uygulamada halen sözleşmelerin vb. evrakın mühürlenmesi talep edilebildiğinden, mühür bastırılmasında da fayda görülmektedir.
+            <p className='font-bold'>2.13. Nitelikli elektronik imza/sertifika temini</p>
+            Belgelerin elektronik olarak imzalanması, sunulması ve onaylanması için nitelikli elektronik imzanın; şirketin yıllık mali tablolarının ve vergi beyanlarının ibraz edilmebilmesi için bir elektronik sertifikanın temin edilmesi gerekmektedir.
+            Bir şirket kurmak ve muhasebe, vergi ve bordro hizmetleri ile ilgili destek sağlamak için güvenilir bir ortağa ihtiyacınız olduğunu varsayıyoruz. Söz konusu hususlar ile diğer tüm faaliyetlerde size yardımcı olacak ekibimizle iletişime geçebilirsiniz.
+            </p>
+          </section>
           <ContactForm />
         </Container>
       </Layout>
